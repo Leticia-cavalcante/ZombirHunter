dd2121a971ddf68e7eec095bdfa40ddb4c8b5576
Leticia <leticia.freitas1534@gmail.com>
2026-06-27 17:06:35 -0300

    Adding classes generated from StarUml

---
+++ code/menu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.Attribute1 = None
+        self.Attribute2 = None
+        self.Attribute3 = None
+        self.Attribute5 = None
+        self.Attribute4 = None
+        self.window = None
+        self.Attribute6 = None
+
+    def run(self, ):
+        pass
